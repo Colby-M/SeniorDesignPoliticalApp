d77ea8b314db9cc32692fcd18e51933c19a20dbb
KYLE_SURFACE\kylem <kylemielke@proton.me>
2023-10-04 16:17:04 -0400

    Fixed Build Issues

--- APolitcalApp/src/router/index.ts
+++ APolitcalApp/src/router/index.ts
@@ -57,14 +57,6 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ViewDevTestPage.vue')
     },
-    {
-      path: '/testicon',
-      name: 'IconTest',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/IconTestView.vue')
-    }
   ]
 })
 
