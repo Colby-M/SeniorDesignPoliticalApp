52100621032d4d08ba85f63e81eb19b278669146
blake-butsko <89292796+blake-butsko@users.noreply.github.com>
2023-10-05 11:23:05 -0400

    Just adding icons rq

--- APolitcalApp/src/views/ViewNewsPage.vue
+++ APolitcalApp/src/views/ViewNewsPage.vue
@@ -47,7 +47,11 @@ import MainLayout from '@/components/layouts/MainLayout.vue';
             </a>
             <!-- Second section -->
             <hr class="col-span-4 my-8">
-            <h5 class="col-span-4 mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">For you</h5>
+            <a href="/#/news" class="col-span-4 mb-4 flex items-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <span>For you</span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-2"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" /></svg>
+            </a>
+
             
             <a href="#" class="col-span-2 p-4 border rounded-lg bg-light2">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
@@ -62,7 +66,10 @@ import MainLayout from '@/components/layouts/MainLayout.vue';
                 <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
             </a>
             <hr class="col-span-4 my-8">
-            <h5 class="col-span-4 mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Education (Tag 1)</h5>
+            <a href="/#/news" class="col-span-4 mb-4 flex items-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <span>Education (Tag1)</span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-2"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" /></svg>
+            </a>
             <a href="#" class="col-span-1 p-4 border rounded-lg bg-light2">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
                 <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
@@ -84,7 +91,10 @@ import MainLayout from '@/components/layouts/MainLayout.vue';
                 <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
             </a>
             <hr class="col-span-4 my-8">
-            <h5 class="col-span-4 mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Culture(Tag 2)</h5>
+            <a href="/#/news" class="col-span-4 mb-4 flex items-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <span>Culture (Tag2)</span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-2"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" /></svg>
+            </a>
             <a href="#" class="col-span-1 p-4 border rounded-lg bg-light2">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
                 <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
@@ -98,7 +108,10 @@ import MainLayout from '@/components/layouts/MainLayout.vue';
                 <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
             </a>
             <hr class="col-span-4 my-8">
-            <h5 class="col-span-4 mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Tag 3</h5>
+            <a href="/#/news" class="col-span-4 mb-4 flex items-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <span>Tag 3</span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-2"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" /></svg>
+            </a>
             <a href="#" class="col-span-1 p-4 border rounded-lg bg-light2">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
                 <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
