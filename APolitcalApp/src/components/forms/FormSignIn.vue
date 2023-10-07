597d153117e103c05bf6c0d737c867bcf78365fa
KYLE_SURFACE\kylem <kylemielke@proton.me>
2023-10-07 12:29:59 -0400

    APP-28 Popup Form UI
    Basic Popup form ui implemented, introduces spacing bug on buttons

---
+++ APolitcalApp/src/components/forms/FormSignIn.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import ButtonSignInOptions from '../button/ButtonSignInOptions.vue';
+</script>
+
+<template>
+
+        <div class="flex flex-col bg-dark/30 items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div class="w-full bg-white rounded-lg bg-light shadow md:mt-0 sm:max-w-md xl:p-0">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <a href="#" class="flex items-center font-light mb-6 text-2xl text-dark dark:text-white">
+                        <img class="w-8 h-8 mr-2" src="../logo/VoteBoatLogo.png" alt="logo">
+                        Vote Boat    
+                    </a>
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-dark md:text-2xl dark:text-white">
+                        Sign in to your account
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" action="#">
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-dark">Your email</label>
+                            <input type="email" name="email" id="email" class="bg-light border border-border text-dark sm:text-sm rounded-lg block w-full p-2.5 focus:border-dark" placeholder="name@company.com" required="true">
+                        </div>
+                        <div>
+                            <label for="password" class="block mb-2 text-sm font-medium text-dark">Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-light border border-border text-dark sm:text-sm rounded-lg block w-full p-2.5 focus:border-dark" required="true">
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <div class="flex items-start">
+                                <div class="flex items-center h-5">
+                                  <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-dark rounded bg-dark focus:ring-3 focus:ring-primary-300 dark:bg-dark dark:border-dark dark:focus:ring-primary-600 dark:ring-offset-dark" required="">
+                                </div>
+                                <div class="ml-3 text-sm">
+                                  <label for="remember" class="text-dark dark:text-dark">Remember me</label>
+                                </div>
+                            </div>
+                            <a href="#" class="text-sm font-light text-dark hover:underline">Forgot password?</a>
+                        </div>
+                        <button type="submit" class="w-1/2 mx-auto bg-light border border-border text-dark sm:text-sm rounded-lg block hover:bg-dark/30 p-2.5 focus:border-dark">Sign in</button>
+                        <div class="flex items-center">
+                          <div class="flex-1 border-b border-border"></div>
+                          <span class="px-2">or</span>
+                          <div class="flex-1 border-b border-border"></div>
+                        </div>
+                        <ButtonSignInOptions buttonType="sign-in-github"></ButtonSignInOptions>
+                    </form>
+                </div>
+            </div>
+        </div>
+</template>
+  
--- APolitcalApp/src/components/layouts/Landing.vue
+++ APolitcalApp/src/components/layouts/Landing.vue
@@ -1,13 +1,22 @@
-<script setup lang="ts">
-import Sidebar_Column from '../nav/Sidebar.vue';
-import NavRow from '../nav/NavRow.vue';
-import ButtonSignIn from '../button/ButtonSignIn.vue';
+<script lang="ts" setup>
+import ButtonSignInOptions from '../button/ButtonSignInOptions.vue';
+import FormSignIn from '../forms/FormSignIn.vue';
+import { ref } from 'vue';
 /* TODO: Upscale The Logo So Edges are Smooth at High Resolution */
 /* TODO: Get Functions for auth from colby */
+const showFormTrigger = ref({
+  buttonTrigger: false
+})
+
 </script>
 
 <template>
-    <div class="grid w-screen h-screen justify-center grid-cols-mobile sm:grid-cols-landing">
+    <div class="w-full h-full absolute">
+        <FormSignIn v-if="true"></FormSignIn>
+    </div>
+
+
+    <div class="grid w-screen h-screen justify-center grid-cols-mobile z-10 sm:grid-cols-landing">
         <div class="hidden sm:flex justify-end border-border my-auto"> 
             <div class="flex items-center h-96">
                 <img src="../logo/VoteBoatLogo.png" class="lg:h-[512px] lg:w-[512px]"/>
@@ -16,14 +25,16 @@ import ButtonSignIn from '../button/ButtonSignIn.vue';
         <div class="my-auto mx-auto sm:mr-auto sm:ml-0">
             <div class="flex flex-col gap-4 items-center justify-center pl-4">
                 <img src="../logo/VoteBoatLogo.png" class="w-44 h-44 sm:hidden"/>
-                <ButtonSignIn buttonType="sign-up-github"></ButtonSignIn>
+                <ButtonSignInOptions buttonType="sign-up-github"></ButtonSignInOptions>
                 <p class="font">or</p>
-                <ButtonSignIn buttonType="sign-up"></ButtonSignIn>
+                <ButtonSignInOptions buttonType="sign-up"></ButtonSignInOptions>
                 <div class="h-2 border-t border-dark w-50"></div>
-                <ButtonSignIn buttonType="sign-in"></ButtonSignIn>            
+                <ButtonSignInOptions buttonType="sign-in"></ButtonSignInOptions>            
             </div>
         </div>
     </div>
+ 
+
 
 
 </template>
