29cc2b511a3d67b0fb189dd77a3d078c1d9ac6de
KYLE_SURFACE\kylem <kylemielke@proton.me>
2023-10-19 11:07:01 -0400

    APP-24 Content Card Base
    Adds base content Card divs.

--- APolitcalApp/src/views/ViewDiscoverPage.vue
+++ APolitcalApp/src/views/ViewDiscoverPage.vue
@@ -6,6 +6,32 @@ import MainLayout from '@/components/layouts/MainLayout.vue';
 <template>
   <MainLayout>
     <template #ContentSlot>Discover
+    <div class="flex flex-wrap gap-8 px-16 items-center justify-center overflow-y-auto">
+      <div class="w-[350px] h-[412px] bg-light group-hover:drop-shadow-2xl duration-700">
+        <div class="group w-[338px] h-[400px] bg-dark  absolute"></div>
+        <div class="group w-[338px] h-[400px] ml-[12px] mt-[12px] bg-highlight absolute">
+          <div class="group w-[314px] h-[376px] m-[12px] border-dashed border-light border"></div>
+        </div>
+      </div>
+      <div class="w-[350px] h-[412px] bg-light group-hover:drop-shadow-2xl duration-700">
+        <div class="group w-[338px] h-[400px] bg-dark  absolute"></div>
+        <div class="group w-[338px] h-[400px] ml-[12px] mt-[12px] bg-highlight absolute">
+          <div class="group w-[314px] h-[376px] m-[12px] border-dashed border-light border"></div>
+        </div>
+      </div>
+      <div class="w-[350px] h-[412px] bg-light group-hover:drop-shadow-2xl duration-700">
+        <div class="group w-[338px] h-[400px] bg-dark  absolute"></div>
+        <div class="group w-[338px] h-[400px] ml-[12px] mt-[12px] bg-highlight absolute">
+          <div class="group w-[314px] h-[376px] m-[12px] border-dashed border-light border"></div>
+        </div>
+      </div>
+      <div class="w-[350px] h-[412px] bg-light group-hover:drop-shadow-2xl duration-700">
+        <div class="group w-[338px] h-[400px] bg-dark  absolute"></div>
+        <div class="group w-[338px] h-[400px] ml-[12px] mt-[12px] bg-highlight absolute">
+          <div class="group w-[314px] h-[376px] m-[12px] border-dashed border-light border"></div>
+        </div>
+      </div>
+    </div>
     </template>
   </MainLayout>
 </template>
