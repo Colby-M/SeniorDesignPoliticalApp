062f43c52efa8e8e2347b6b61164a163e22649bb
DESKTOP-GCBAMJU\charl <charley.walter82@yahoo.com>
2023-10-25 14:55:19 -0400

    Added toolbars for discover, Review, Settings, profile, profileOther

---
+++ APolitcalApp/src/components/toolbars/toolbarSettings.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import toolbarBase from './toolbarBase.vue'
+import ButtonBase from '../button/ButtonBase.vue'
+import SearchBar from '../search/SearchBar.vue'
+
+
+
+const filterClicked = () => {
+    //Add Refresh Function
+}
+
+</script>
+
+<template>
+    <toolbarBase>
+        <template #toolbar-buttons-lg>
+            <ButtonBase buttonType="filter" @filter="filterClicked"></ButtonBase>
+            
+        </template>
+
+        <template #toolbar-buttons-sm>
+            <ButtonBase buttonType="filter" @filter="filterClicked"></ButtonBase>
+            
+        </template>
+
+        
+    </toolbarBase>
+</template>
--- APolitcalApp/src/views/ViewReviewPage.vue
+++ APolitcalApp/src/views/ViewReviewPage.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import MainLayout from '@/components/layouts/MainLayout.vue';
+import ToolbarReview from '../components/toolbars/toolbarReview.vue'
 </script>
 
 <template>
   <MainLayout>
+    <template #ToolbarSlot>
+      <ToolbarReview></ToolbarReview>
+    </template>
+
     <template #ContentSlot>Review</template>
   </MainLayout>
 </template>
