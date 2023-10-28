7a6aa53c9d21441808b175c8d774d10e0be9f6d5
KYLE_SURFACE\kylem <kylemielke@proton.me>
2023-10-28 10:45:49 -0400

    APP-36 Adds Review Petition Form
    Adds full review petition form

--- APolitcalApp/src/components/cards/CardFullPetitionReview.vue
+++ APolitcalApp/src/components/cards/CardFullPetitionReview.vue
@@ -1,11 +1,8 @@
 <script lang="ts" setup>
 import { computed } from 'vue';
 
+import goalSlider from '../slider/goalSlider.vue'
 import IconBase from '../icons/IconBase.vue';
-import ButtonBase from '../button/ButtonBase.vue'
-
-import router from '@/router';
-import type { RouteLocationRaw } from 'vue-router';
 
 const emit = defineEmits()
 
@@ -20,88 +17,71 @@ interface petitionTypeReview {
   petitionLocked:boolean;
 }
 
+const props = defineProps<petitionTypeReview>(); 
+
 const goalString = computed( () => 
   {
     return ( `${props.petitionGoal} of ${props.petitionGoal} signatures`) 
   }
 )
 
-const props = defineProps<petitionTypeReview>(); 
-
-
-
-const denyPetition = () => {
-  emit('test')
-}
-
-const approvePetition = () => {
-  //TODO: Add approve database call, save petition id to history database
-}
-
 </script>
 
 <template>
-    <div class="flex flex-col items-center bg-dark sm:bg-dark/50 justify-center px-6 py-8 mx-auto h-full w-full">
-        <div class="bg-white rounded-lg border border-border bg-light drop-shadow-sm w-full max-w-md overflow-y-auto">
-            <div class="p-8 space-y-6">
-                <div href="#" class="flex row font-light text-start mb-6 text-2xl justify-between text-dark">
-                    <div class="flex items-center">
-                        <img class="w-8 h-8 mr-2" src="../logo/VoteBoatLogo.png" alt="logo">
-                        Create Petition
-                    </div>                    
-                </div>
-                <form class="space-y-6 items-center justify-center mx-auto" action="#">
-                    
-                    <!-- Petition Title -->
-                    <label class="">Title
-                        <input type="title" name="title" id="petitionTitle" maxlength="25" v-model="props.petitionTitle" class="bg-light border border-border text-dark text-sm rounded-lg block w-full p-2.5 focus:border-dark" placeholder="petition title...">                      
-                    </label> 
 
-                    <!-- Petition Summary -->
-                    <label name="summary"> Summary
-                      <textarea name="summary" maxlength="512" id="petition-summary" v-model="props.petitionSummary" class="bg-light border h-32 border-border text-dark text-sm top-1 rounded-lg block w-full p-2.5 focus:border-dark" placeholder="petition title..."></textarea>                       
-                    </label>
-
-                    <div class="flex-col items-center space-y-2 sm:flex sm:flex-row sm:gap-3 sm:space-y-0">    
-                        <!-- Petition Goal -->
-                        <div class="flex flex-col">
-                            <input name="goal" id="goal" min="0" v-model="goalString" class="bg-light border border-border text-dark text-sm rounded-lg p-2.5 block focus:border-dark" placeholder="goal">                       
-                        </div>
-                      
-                        <!-- Petition Tags -->
-
-                    <!-- Generated Tag Elements -->
-                    <div class="flex flex-wrap gap-1 justify-center">
-                        <div v-for="element in props.petitionTags" class="flex flex-row border px-1 items-center border-border rounded-full hover:bg-dark/10">
-                            <div class="flex items-center">
-                                <p class="font-light text-dark">{{ element }}</p>
-                                <IconBase iconColor="animate-dark" iconName="exit" iconSize="small"/>
-                            </div>                      
-                        </div>
-                    </div>
-
-                    <!-- Petition Scope Elements -->
-                    <div class="flex flex-col">
-                        <label>Petition Scope
-                            <input id="steps-range" :value="goalString" type="range" min="1"  max="4" step="1" class="w-full accent-highlight h-3 thumb-full bg-light rounded-lg border border-border cursor-pointer">
-                        </label>
-                        <div class="flex justify-between mt-0.5">
-                            <span class="font-light text-dark">local</span>
-                            <span class="font-light text-dark">regional</span>
-                            <span class="font-light text-dark">national</span>
-                            <span class="font-light text-dark">global</span>
-                        </div>
-                    </div>
-
-                    <!-- Petition Save And Submit Buttons -->
-                    <div class="flex flex-col text-center text-dark text-sm gap-2">
-                        <div class="flex flex-row gap-2">
-
-                        </div>
-                    </div>
-                    </div>
-                </form>
-            </div>
+    <div class="bg-white rounded-lg border border-border bg-light drop-shadow-sm w-[448px] overflow-y-auto">
+      <div class="p-8 space-y-6">
+        <div href="#" class="flex row font-light text-start mb-6 text-xl justify-between text-dark">
+          <div class="flex items-center">
+            <img class="w-8 h-8 mr-2" src="../logo/VoteBoatLogo.png" alt="logo">
+            {{props.petitionTitle}}
+          </div>                    
         </div>
+        <form class="space-y-6 items-center justify-center mx-auto" action="#">           
+          <!-- Petition Summary -->
+          <label name="summary"> 
+            <div class="flex items-center gap-2">
+              <p>Solution</p>
+              <IconBase v-if="props.petitionLocked" iconName="locked" iconColor="static-dark" iconSize="small"></IconBase>
+              <IconBase v-if="!props.petitionLocked" iconName="unlocked" iconColor="static-dark" iconSize="small"></IconBase>
+            </div>
+            <textarea name="summary" readonly maxlength="512" id="petition-summary" v-model="props.petitionSummary" class="bg-light border h-32 border-border text-dark text-sm top-1 rounded-lg block w-full p-2.5 focus:border-dark" placeholder="petition title..."></textarea>                       
+          </label>
+
+          <!-- Petition Goal -->
+          <div class="flex flex-col">
+            <goalSlider :currentNumber="$props.petitionSignatures" :goalNumber="props.petitionGoal"></goalSlider>
+          </div>
+
+          <!-- Petition Scope Elements -->
+          <div class="flex flex-col">
+              <label>Petition Scope
+                  <input id="steps-range" disabled="true" readonly :value="goalString" type="range" min="1"  max="4" step="1" class="w-full accent-highlight h-3 thumb-full bg-light rounded-lg border border-border cursor-pointer">
+              </label>
+              <div class="flex justify-between mt-0.5">
+                  <span class="font-light text-dark">local</span>
+                  <span class="font-light text-dark">regional</span>
+                  <span class="font-light text-dark">national</span>
+                  <span class="font-light text-dark">global</span>
+              </div>
+          </div>
+
+          <!-- Generated Tag Elements -->
+          <div class="flex flex-wrap gap-1 justify-center">
+            <div v-for="element in props.petitionTags" class="flex flex-row border px-2 items-center border-border rounded-full hover:bg-dark/10">
+              <div class="flex items-center">
+                  <p class="font-light text-dark">{{ element }}</p>
+              </div>                      
+            </div>
+          </div>
+
+          <!-- Petition Save And Submit Buttons -->
+          <div class="flex flex-col text-center text-dark text-sm gap-2">
+              <div class="flex flex-row gap-2">
+              </div>
+            </div>
+        </form>
+      </div>
     </div>
+
 </template>
--- APolitcalApp/src/views/ViewReviewPage.vue
+++ APolitcalApp/src/views/ViewReviewPage.vue
@@ -18,7 +18,7 @@ const sampleTagArray = ['tag1', 'tag2', 'tag3', 'tag5']
 
 const testData: petitionTypeReview[] = [
   { petitionId: '1',  petitionTitle: 'Title 1',  petitionSummary: testSummary,  petitionLocked: false, petitionScope: 0, petitionTags: sampleTagArray, petitionGoal: 1200},
-  { petitionId: '2',  petitionTitle: 'Title 2',  petitionSummary: testSummary,  petitionLocked: true , petitionScope: 2, petitionTags: sampleTagArray, petitionGoal: 1200},
+  { petitionId: '2',  petitionTitle: 'Petition Title 1',  petitionSummary: testSummary,  petitionLocked: true , petitionScope: 2, petitionTags: sampleTagArray, petitionGoal: 1423},
   { petitionId: '3',  petitionTitle: 'Title 3',  petitionSummary: testSummary,  petitionLocked: false, petitionScope: 3, petitionTags: sampleTagArray, petitionGoal: 1200},
   { petitionId: '4',  petitionTitle: 'Title 4',  petitionSummary: testSummary,  petitionLocked: true , petitionScope: 1, petitionTags: sampleTagArray, petitionGoal: 1200},
   { petitionId: '5',  petitionTitle: 'Title 5',  petitionSummary: testSummary,  petitionLocked: false, petitionScope: 2, petitionTags: sampleTagArray, petitionGoal: 1200},
@@ -38,12 +38,17 @@ const testData: petitionTypeReview[] = [
       <ToolbarReview></ToolbarReview>
     </template>
     <template #ContentSlot>
-      <div id="discoverScroll" class="max-h-[100vh] overflow-y-auto" ref="scrollComponent">
-        <CardFullPetitionReview :petitionTitle="testData[1].petitionTitle" :petitionGoal="testData[1].petitionGoal" 
-                                :petitionId="testData[1].petitionId" :petitionLocked="testData[1].petitionLocked"  
-                                :petitionScope="testData[1].petitionScope" :petitionSummary="testData[1].petitionSummary"  
-                                :petitionTags="testData[1].petitionTags" :petitionSignatures="100"/>
+      <div id="discoverScroll" class="max-h-[100vh] w-full overflow-y-auto py-2" ref="scrollComponent">
+        <div class="flex flex-col gap-2 items-center justify-center">
+          <CardFullPetitionReview :petitionTitle="testData[1].petitionTitle" :petitionGoal="testData[1].petitionGoal" 
+                                  :petitionId="testData[1].petitionId" :petitionLocked="testData[1].petitionLocked"  
+                                  :petitionScope="testData[1].petitionScope" :petitionSummary="testData[1].petitionSummary"  
+                                  :petitionTags="testData[1].petitionTags" :petitionSignatures="1600"/>
+          <div class="w-full border-t border-dashed px-6 border-border h-96">
 
+
+          </div>
+        </div>
       </div>
     </template>
   </MainLayout>
