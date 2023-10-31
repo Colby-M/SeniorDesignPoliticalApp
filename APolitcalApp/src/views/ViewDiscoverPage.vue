e19f281e45974e9cf167de9f74e3934c4683449e
Colby McDonald <67612969+Colby-M@users.noreply.github.com>
2023-10-30 22:08:24 -0400

    comment out to get rid of errors

--- APolitcalApp/src/views/ViewDiscoverPage.vue
+++ APolitcalApp/src/views/ViewDiscoverPage.vue
@@ -20,27 +20,27 @@ interface petitionType {
 
 const isLoadingPosts = ref(false)
 const postArray = ref<petitionType[]>([])
-const testSummary = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum iaculis eu non diam phasellus vestibulum lorem. Diam in arcu cursus euismod quis. Nunc non blandit massa enim nec dui nunc. Tincidunt eget nullam non nisi est sit amet facilisis magna.'
+//const testSummary = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum iaculis eu non diam phasellus vestibulum lorem. Diam in arcu cursus euismod quis. Nunc non blandit massa enim nec dui nunc. Tincidunt eget nullam non nisi est sit amet facilisis magna.'
 
-const testData: petitionType[] = [
-  { petitionId: '1',  petitionTitle: 'Title 1',  petitionSummary: testSummary,  petitionLocked: false },
-  { petitionId: '2',  petitionTitle: 'Title 2',  petitionSummary: testSummary,  petitionLocked: true  },
-  { petitionId: '3',  petitionTitle: 'Title 3',  petitionSummary: testSummary,  petitionLocked: false },
-  { petitionId: '4',  petitionTitle: 'Title 4',  petitionSummary: testSummary,  petitionLocked: true  },
-  { petitionId: '5',  petitionTitle: 'Title 5',  petitionSummary: testSummary,  petitionLocked: false },
-  { petitionId: '6',  petitionTitle: 'Title 6',  petitionSummary: testSummary,  petitionLocked: true  },
-  { petitionId: '7',  petitionTitle: 'Title 7',  petitionSummary: testSummary,  petitionLocked: false },
-  { petitionId: '8',  petitionTitle: 'Title 8',  petitionSummary: testSummary,  petitionLocked: true  },
-  { petitionId: '9',  petitionTitle: 'Title 9',  petitionSummary: testSummary,  petitionLocked: false },
-  { petitionId: '10', petitionTitle: 'Title 10', petitionSummary: testSummary,  petitionLocked: true  },
+//const testData: petitionType[] = [
+  //{ petitionId: '1',  petitionTitle: 'Title 1',  petitionSummary: testSummary,  petitionLocked: false },
+  //{ petitionId: '2',  petitionTitle: 'Title 2',  petitionSummary: testSummary,  petitionLocked: true  },
+  //{ petitionId: '3',  petitionTitle: 'Title 3',  petitionSummary: testSummary,  petitionLocked: false },
+  //{ petitionId: '4',  petitionTitle: 'Title 4',  petitionSummary: testSummary,  petitionLocked: true  },
+  //{ petitionId: '5',  petitionTitle: 'Title 5',  petitionSummary: testSummary,  petitionLocked: false },
+  //{ petitionId: '6',  petitionTitle: 'Title 6',  petitionSummary: testSummary,  petitionLocked: true  },
+  //{ petitionId: '7',  petitionTitle: 'Title 7',  petitionSummary: testSummary,  petitionLocked: false },
+  //{ petitionId: '8',  petitionTitle: 'Title 8',  petitionSummary: testSummary,  petitionLocked: true  },
+  //{ petitionId: '9',  petitionTitle: 'Title 9',  petitionSummary: testSummary,  petitionLocked: false },
+  //{ petitionId: '10', petitionTitle: 'Title 10', petitionSummary: testSummary,  petitionLocked: true  },
   // ... add more test data as required
-];
+//];
 
-onMounted(() => {
+onMounted(async () => {
   let internalDiv = <HTMLElement>document.getElementById("discoverScroll");
   if(internalDiv !== null)
   {
-    getPosts(10);
+    await getPosts(10);
     internalDiv.addEventListener("scroll", handleScroll)
   }
 
