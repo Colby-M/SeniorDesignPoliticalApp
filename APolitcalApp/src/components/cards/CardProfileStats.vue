95785aec6a35bf62504c26f0d55aec53dd5621ae
DESKTOP-GCBAMJU\charl <charley.walter82@yahoo.com>
2023-11-01 08:47:14 -0400

    APP-30 Setup for Profile UI

---
+++ APolitcalApp/src/components/cards/CardProfileStats.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+<RouterLink to="linkToPetition">
+    <div class="group flex flex-row w-64 h-14 border-dark rounded-full border gap-4 items-center bg-light hover:border-dark duration-300">
+        <div class="w-14 h-14 flex rounded-full items-center justify-center bg-dark">
+            <!-- TODO Replace Icon Base with link to users profile image (Work with backend to define this process) -->
+            <IconBase iconColor="static-light" iconName="locked" iconSize="large"></IconBase>
+        </div>
+        <span class="text-dark font-normal text-sm">flipper</span>
+    </div>
+    </RouterLink>
+
+</template>
--- APolitcalApp/src/views/ViewProfilePage.vue
+++ APolitcalApp/src/views/ViewProfilePage.vue
@@ -2,8 +2,16 @@
   import AuthAccount from '@/components/Auth/AuthAccount.vue';
   import MainLayout from '@/components/layouts/MainLayout.vue';
   import ToolbarProfilePersonal from '../components/toolbars/toolbarProfilePersonal.vue'
+  import CardFullPetition from '@/components/cards/CardFullPetition.vue';
+  import CardProfileName  from '@/components/cards/CardProfileNameBio.vue'
   import { useAuthStore } from '@/stores/Auth';
-
+  import { reactive } from 'vue';
+  const testPetitionObject = reactive({
+  petitionTitle: '',
+  petitionId: 'test2',
+  petitionSummary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  petitionLocked: true,
+})
   const authStore = useAuthStore();
 </script>
 
@@ -13,8 +21,8 @@
       <ToolbarProfilePersonal></ToolbarProfilePersonal>
     </template>
     <template #ContentSlot>
-      <h1>profile</h1>
-      <AuthAccount @sign-out="authStore.logout()" v-if="authStore.session" :session="authStore.session" />
+      <CardFullPetition :petitionObject="testPetitionObject"></CardFullPetition>
+      <CardProfileName></CardProfileName>
     </template>
   </MainLayout>
 </template>
