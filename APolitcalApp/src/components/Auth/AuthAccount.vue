8c12fc61b8e254aaa64f9a461dc3a2e5d347a911
Colby McDonald <67612969+Colby-M@users.noreply.github.com>
2023-11-06 17:10:30 -0500

    show pettions and solutions in profile page

--- APolitcalApp/src/components/Auth/AuthAccount.vue
+++ APolitcalApp/src/components/Auth/AuthAccount.vue
@@ -72,31 +72,33 @@ function signOut()
 </script>
 
 <template>
-  <form class="form-widget" @submit.prevent="updateProfile">
-    <div>
-      <label for="email">Email</label>
-      <input id="email" type="text" :value="session.user.email" disabled />
+  <form class="flex flex-col gap-2" @submit.prevent="updateProfile">
+    <div class="flex gap-3">
+      <label for="email" class="label">Email</label>
+      <input id="email" type="text" class="bg-light border p-1" :value="session.user.email" disabled />
     </div>
-    <div>
+    <div class="flex gap-2">
       <label for="username">Name</label>
-      <input id="username" type="text" v-model="username" />
+      <input id="username" type="text" class="bg-light border p-1" v-model="username" />
     </div>
-    <div>
+    <div class="flex gap-5">
       <label for="website">Age</label>
-      <input id="website" type="number" v-model="age" />
+      <input id="website" type="number" class="bg-light border p-1" v-model="age" />
     </div>
-
-    <div>
-      <input
+    <div class="flex gap-5">
+      <button
         type="submit"
-        class="button primary block"
-        :value="loading ? 'Loading ...' : 'Update'"
+        class="w-fit text-light bg-highlight hover:bg-highlight/90  h-10 focus:ring-4 focus:outline-none focus:ring-highlight/50 font-medium rounded-full text-xs px-5 py-2.5 text-center dark:focus:ring-[#4285F4]/55"
         :disabled="loading"
-      />
-    </div>
-
-    <div>
-      <button class="button block" @click="signOut()" :disabled="loading">Sign Out</button>
+      >{{ loading ? 'Loading ...' : 'Update' }}</button>
+  
+      <div>
+        <button
+          @click="signOut()"
+          :disabled="loading"
+          class="w-fit text-light bg-[#ff4545] hover:bg-[#ba3838]  h-10 focus:ring-4 focus:outline-none focus:ring-[#ff4545]/50 font-medium rounded-full text-xs px-5 py-2.5 text-center dark:focus:ring-[#ff4545]/55"
+        >Sign Out</button>
+      </div>
     </div>
   </form>
 </template>
--- APolitcalApp/src/views/ViewProfilePage.vue
+++ APolitcalApp/src/views/ViewProfilePage.vue
@@ -2,9 +2,32 @@
   import AuthAccount from '@/components/Auth/AuthAccount.vue';
   import MainLayout from '@/components/layouts/MainLayout.vue';
   import ToolbarProfilePersonal from '../components/toolbars/toolbarProfilePersonal.vue'
+  import CardPetitionTitle from '@/components/cards/CardPetitionTitle.vue';
+  import CardSolutionsOtherSuggestions from '@/components/cards/CardSolutionsOtherSuggestions.vue';
   import { useAuthStore } from '@/stores/Auth';
+  import { onMounted, ref } from 'vue';
+  import supabase from '@/lib/supabaseClient';
+  import type IPetitionType from '../interfaces/IPetitionType';
+  import type ISolutionType from '../interfaces/ISolutionType';
 
   const authStore = useAuthStore();
+  const petitions = ref<IPetitionType[]>();
+  const solutions = ref<ISolutionType[]>();
+  onMounted(async () => {
+    // get our own petitions
+    petitions.value = (await supabase
+    .from('Petitions')
+    .select<"*", IPetitionType>()
+    .eq('userid', authStore.session?.user.id)).data ?? [];
+
+    // get our own solutions
+    solutions.value = (await supabase
+    .from('Solution')
+    .select<"*", ISolutionType>()
+    .eq('userid', authStore.session?.user.id)).data ?? [];
+
+    console.log(petitions.value, solutions.value);
+  });
 </script>
 
 <template>
@@ -13,8 +36,23 @@
       <ToolbarProfilePersonal></ToolbarProfilePersonal>
     </template>
     <template #ContentSlot>
-      <h1>profile</h1>
-      <AuthAccount @sign-out="authStore.logout()" v-if="authStore.session" :session="authStore.session" />
+      <div class="flex flex-col gap-5">
+        <AuthAccount @sign-out="authStore.logout()" v-if="authStore.session" :session="authStore.session" />
+        <div class="flex w-full justify-between">
+          <div>
+            <p>Your Petitions</p>
+            <div class="flex flex-col gap-2">
+              <CardPetitionTitle v-for="petition in petitions" :key="petition.id" :petition-id="petition.id" :petition-title="petition.title" />
+            </div>
+          </div>
+          <div>
+            <p>Your Solutions</p>
+            <div class="flex flex-col gap-2">
+              <CardSolutionsOtherSuggestions v-for="post in solutions" :linkedPetition="post.id" :suggestionText="post.description" :uservotes="post.uservotes.length ?? 0" />
+            </div>
+          </div>
+        </div>
+      </div>
     </template>
   </MainLayout>
 </template>
