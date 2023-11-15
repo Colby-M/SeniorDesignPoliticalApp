55112a0e8d859781f14c5594bc66251e6390794b
KYLE_SURFACE\kylem <kylemielke@proton.me>
2023-11-15 09:41:53 -0500

    APP-46 Profanity Filter on Suggestions
    Adds profanity filter to suggestions

--- APolitcalApp/src/components/cards/CardSolutionsPersonalSuggestion.vue
+++ APolitcalApp/src/components/cards/CardSolutionsPersonalSuggestion.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue';
 import ButtonBase from '../button/ButtonBase.vue';
 import supabase from '@/lib/supabaseClient';
 import { useAuthStore } from '@/stores/Auth';
-import {NinjasAPIProfFilter} from '../../lib/ninjaClient';
+import NinjasAPIProfFilter from '../../lib/ninjaClient';
 import type IFilterType from '../../interfaces/IFilterType'
 
 const props = defineProps({
@@ -16,18 +16,19 @@ const props = defineProps({
 
 const loadingSend = ref(false);
 const suggestion = ref('');
-const responseTest = ref()
+const profanityDetected = ref(false);
 
 async function submitSuggestion(){
     //TODO Error handling
     let formattedSuggestion = suggestion.value.trim()
 
-    responseTest.value = NinjasAPIProfFilter(formattedSuggestion)
+    let containsProfanity = await NinjasAPIProfFilter(formattedSuggestion)
 
-    if (responseTest.has_profanity === true) {
-        console.log('profanity detected')
+    if (containsProfanity === true) {
+        profanityDetected.value = true;
+        suggestion.value = ''
+        return
     }
-    /*
     if (formattedSuggestion !== ''){
         loadingSend.value = true
         const res = await supabase.from('Solution').insert({ title: formattedSuggestion, description: formattedSuggestion, petitionid: props.linkedPetition, userid: useAuthStore().session?.user.id});
@@ -35,15 +36,16 @@ async function submitSuggestion(){
         setTimeout(() => {
             loadingSend.value = false;
         }, 750);
+        profanityDetected.value = false
         suggestion.value = ''
 
         //Emit event to re-render suggestions to include this one in the section
         emitReRenderEvent()
     }
 
-    else{ */
+    else{ 
         /* Do Nothing */
-    //}
+    }
     
 };
 const emit = defineEmits();
@@ -56,12 +58,19 @@ async function discardSuggestion() {
     suggestion.value = ''
 };
 
+const checkPlaceholderText = () => {
+    if (profanityDetected.value === true) {
+        return 'profanity detected'
+    }
+    return 'your suggestion...'
+}
+
 </script>
 
 <template>
     <div class="w-[91%] xxs:w-96 h-38 xxs:h-64">
         <div class="w-full h-36 xxs:h-56 border border-dashed items-center justify-center border-dark rounded-lg">
-            <textarea class="rounded-lg w-full h-56 border border-dashed bg-light-highlight px-2 text-center resize-none" v-model="suggestion" placeholder="your suggestion..." />
+            <textarea class="rounded-lg w-full h-56 border border-dashed bg-light-highlight px-2 text-center resize-none" v-model="suggestion" :placeholder="checkPlaceholderText()" />
         </div>
         <div class="w-full flex relative bottom-5 items-end flex-row gap-2 justify-end px-2">
             <ButtonBase buttonType="discard-xs" @discard="discardSuggestion"></ButtonBase>
--- APolitcalApp/src/lib/ninjaClient.ts
+++ APolitcalApp/src/lib/ninjaClient.ts
@@ -1,5 +1,5 @@
 
-export const NinjasAPIProfFilter = (text: string) => {
+export default async function NinjasAPIProfFilter(text: string) {
 let options = {
     method: 'GET',
     headers: { 'x-api-key': 'qSLT7pGOmE3dTm+Xk0wtDA==YTbvaQqRtkBlnXar' }
@@ -7,6 +7,15 @@ let options = {
   
   let url = 'https://api.api-ninjas.com/v1/profanityfilter?text=' + text;
   
-  fetch(url,options).then(res => res.json()).then(data => {console.log(data)}).catch(err => {console.log(`error ${err}`)}); 
+  const fetchedData = fetch(url,options)
+                        .then(response => response.json())
+                        .then(data => {
+                          return data.has_profanity
+                        });
+
+  return (fetchedData)
 
 }
+
+
+
