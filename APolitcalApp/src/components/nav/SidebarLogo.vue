7866e19afb3b0802f5c92269e292082ff8d8a14d
KYLE_SURFACE\kylem <kylemielke@proton.me>
2023-12-04 07:58:00 -0500

    Removes settings, adds beta to logo

--- APolitcalApp/src/components/nav/SidebarLogo.vue
+++ APolitcalApp/src/components/nav/SidebarLogo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="SidebarElementContainer flex justify-center px-2 py-6 lg:justify-start lg:gap-1 lg:pr-8"> 
-        <img src="../logo/testlogo2.png" height="32" width="32"/>
+    <div class="SidebarElementContainer flex text-sm font-extralight italic text-dark justify-center px-2 py-6 lg:justify-start lg:gap-1 lg:pr-8"> 
+        <img src="../logo/testlogo2.png" height="32" width="32"/>Beta
     </div>
 </template>
